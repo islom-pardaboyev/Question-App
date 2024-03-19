2bfe18c6c27a28f5fa2e0627ba993423d833f1ab
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-03-19 15:19:39 +0500

    Updated file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        white: {
-          DEFAULT: "#F6F2FF",
-          100: "#fff",
+        primary: {
+          100: "#F6F2FF",
+          200: "#6B38FE",
+          300: "#1A0C40",
         },
-        purple: {
-          DEFAULT: "#6B38FE",
-          100: "#6B38FE40",
+        gray: {
+          DEFAULT: "#EFEFEF",
         },
-        dark: {
-          DEFAULT: "#1A0C40",
+        green: {
+          DEFAULT: "#1AE477",
         },
-        grey: {
-          DEFAULT: '#EFEFEF'
-        }
+        sky: {
+          DEFAULT: "#3893FE",
+        },
+      },
+      boxShadow: {
+        card: "0 4px 10px 0 rgba(180, 175, 192, 0.25)",
+        purpleBtn: "0 4px 11px 0 rgba(107, 56, 254, 0.25)",
       },
     },
   },
